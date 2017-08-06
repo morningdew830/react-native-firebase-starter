af2c61d0d46b2a7bc471dfd7d8536b77f833c109
Ken Kosaki <morningdew830@mail.com>
2017-08-07 04:52:40 +0900

    initial commit

--- src/firebase.js
+++ src/firebase.js
@@ -2,9 +2,11 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: "xxxxx",
-  authDomain: "xxxx.firebaseapp.com",
-  databaseURL: "https://xxxx.firebaseio.com",
-  storageBucket: "xxxx.appspot.com",
+  apiKey: "AIzaSyAM7zJHcl_q3yv2xfvQO8bkBS7pOEptp4k",
+  authDomain: "newapp-f22cf.firebaseapp.com",
+  databaseURL: "https://newapp-f22cf.firebaseio.com",
+  projectId: "newapp-f22cf",
+  storageBucket: "newapp-f22cf.appspot.com",
+  messagingSenderId: "755393795632"
 }
 export const firebaseApp = firebase.initializeApp(firebaseConfig)
